ea3c2b89c251ab371022d61c01e3d4b801988cb5
akinozsoy <akn.ozsy5757@gmail.com>
2022-09-16 15:13:49 +0300

    Java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String ortaMetin = "İlginizi Çekebilir";
+        System.out.println("Hello World");
+        String altMetin = "Vade Süresi";
+        System.out.println(ortaMetin);
+        System.out.println(altMetin);
+        //integer
+        int vade = 12;
+        // Ondalikli degerler.
+        double dolarDun = 18.5;
+
+        double dolarBugun = 18.10;
+        boolean dolarDustuMu = true; //bu değerler true ya da false dönebilir.
+
+
+    }
+}
