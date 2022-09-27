92e0dc380f71f67e04d36a83b1c3566bdc2640fd
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-09-27 15:32:54 +0300

    Java Ders

---
+++ src/oop1/Main.java
@@ -0,0 +1,18 @@
+package oop1;
+
+public class Main {
+ public static void main(String[] args){
+     Product product1 = new Product();
+
+                    //Set Value
+     product1.name = "Delonghi Kahve Makinesi";
+     product1.unitPrice = 7500;
+     product1.discount = 7;
+     product1.unitsInStock = 3;
+     product1.imageUrl = "bilmemne.jpg";
+
+                    // Get
+     System.out.println(product1.name);
+ }
+
+}
