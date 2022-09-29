41a4a1c3ff51361fb3f3e2d52a2f097b3e861782
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-09-29 08:09:19 +0300

    Java Ders

--- src/oop1/Main.java
+++ src/oop1/Main.java
@@ -3,40 +3,37 @@ package oop1;
 public class Main {
     public static void main(String[] args) {
         Product product1 = new Product();
-        //Set Value
-        product1.name = "Delonghi Kahve Makinesi";
-        product1.unitPrice = 7500;
-        product1.discount = 8;
-        product1.unitsInStock = 5;
-        product1.imageUrl = "bilmemne1.jpg";
+        //Set Value YAZMAK İÇİN SET YAPIYORUZ.
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setImageUrl("image1.jpg");
 
-        // Get
+        // Get OKUMAK İÇİN GET YAPIYORUZ.
 
 
         Product product2 = new Product();
-        //Set Value
-        product2.name = "smeg Kahve Makinesi";
-        product2.unitPrice = 7500;
-        product2.discount = 7;
-        product2.unitsInStock = 3;
-        product2.imageUrl = "bilmemne2.jpg";
+        //Set Value YAZMAK İÇİN SET YAPIYORUZ.
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setDiscount(7);
+        product2.setUnitPrice(7300);
+        product2.setImageUrl("image2.jpg");
 
-        // Get
+        // Get OKUMAK İÇİN GET YAPIYORUZ.
 
         Product product3 = new Product();
-        //Set Value
-        product3.name = "asdas Kahve Makinesi";
-        product3.unitPrice = 7500;
-        product3.discount = 7;
-        product3.unitsInStock = 3;
-        product3.imageUrl = "bilmemne3.jpg";
+        //Set Value YAZMAK İÇİN SET YAPIYORUZ.
+        product3.setName("Kitchen aid Kahve Makinesi");
+        product3.setDiscount(2);
+        product3.setUnitPrice(5300);
+        product3.setImageUrl("image3.jpg");
 
-        // Get
+        // Get OKUMAK İÇİN GET YAPIYORUZ.
 
         Product[] products = {product1, product2, product3};
         System.out.println("<ul>");
         for (Product product : products) {
-            System.out.println("<li>" + product.name + "</li>");
+            System.out.println("<li>" + product.getName() + "</li>");
 
         }
 
