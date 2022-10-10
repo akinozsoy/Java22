5008eb1910ef22df2bbb439c37c51a9a97163967
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-10-10 11:48:05 +0300

    JAVA 22

--- oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,15 +1,22 @@
 package oopWithNLayeredApp;
 
 import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.core.logging.DatabaseLogger;
+import oopWithNLayeredApp.core.logging.FileLogger;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.core.logging.MailLogger;
 import oopWithNLayeredApp.dataAcces.HibernateProductDao;
 import oopWithNLayeredApp.dataAcces.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Product product1 = new Product(1,"Iphone Xr",9000);
+        Logger[] loggers = {new DatabaseLogger(),new FileLogger(),new MailLogger()};
 
-        ProductManager productManager = new ProductManager(new HibernateProductDao());
+        ProductManager productManager = new ProductManager(new HibernateProductDao(), List.of(loggers));
         productManager.add(product1);
     }
 }
--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,14 +1,19 @@
 package oopWithNLayeredApp.business;
 
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAcces.HibernateProductDao;
 import oopWithNLayeredApp.dataAcces.JdbcProductDao;
 import oopWithNLayeredApp.dataAcces.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
+import java.util.List;
+
 public class ProductManager {
     private ProductDao productDao;
-    public ProductManager(ProductDao productDao){
+    private List<Logger> loggers;
+    public ProductManager(ProductDao productDao, List<Logger> loggers){
         this.productDao = productDao;
+        this.loggers = loggers;
     }
     public void add(Product product) throws Exception{ //response request
     //İŞ KURALLARI
@@ -16,5 +21,9 @@ public class ProductManager {
             throw new Exception("Ürün fiyatı 10 dan küçük olamaz");
         }
         productDao.add(product);
+        for(Logger logger: loggers){ //[db,mail]-
+            logger.log(product.getName());
+
+        }
     }
 }
